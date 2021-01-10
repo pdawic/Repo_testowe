80e78c66f4d3f80b03f1dd35b721f45c4cb6e018
Paweł <55764496+pdawic@users.noreply.github.com>
2021-01-10 08:14:20 +0100

    initial

---
+++ drugi_plik.py
@@ -0,0 +1,7 @@
+from main import print_hi
+
+print_hi('test')
+
+
+
+
---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+def print_hi(name):
+    print(name)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    import os
+    os.path.join(os.getcwd(), 'podfolder1')
+    #todo :blee
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
